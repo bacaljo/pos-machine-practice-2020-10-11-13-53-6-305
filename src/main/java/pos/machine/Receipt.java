053c02ddb42cc16ed5bc64fba66a304ab871a1a8
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-13 21:25:09 +0800

    [POS Machine] [John Emmanuel] feature: complete attributes and methods of Receipt

--- src/main/java/pos/machine/Receipt.java
+++ src/main/java/pos/machine/Receipt.java
@@ -1,16 +1,32 @@
 package pos.machine;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static pos.machine.PosMachine.CURRENCY;
 
 public class Receipt {
     private List<ReceiptItem> receiptItemList;
+    private int total;
 
     public Receipt(List<ReceiptItem> receiptItemList) {
         this.receiptItemList = receiptItemList;
+        for (ReceiptItem receiptItem : receiptItemList) {
+            total += receiptItem.getSubtotal();
+        }
     }
 
     @Override
     public String toString() {
-        return null;
+        List<String> receiptLineList = new ArrayList<>();
+
+        receiptLineList.add("***<store earning no money>Receipt ***");
+        for (ReceiptItem receiptItem : receiptItemList) {
+            receiptLineList.add(receiptItem.toString());
+        }
+        receiptLineList.add("----------------------");
+        receiptLineList.add(String.format("Total: %d (%s)", total, CURRENCY));
+
+        return String.join("\r\n", receiptLineList);
     }
 }
