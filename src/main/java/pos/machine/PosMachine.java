3fae7c88cf3166ce52754c9ad69b0796f1df3115
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-13 20:24:58 +0800

    [POS Machine] [John Emmanuel] feature: create getItemQuantity(), buildReceiptItems() and createReceipt() methods to complete logic of PosMachine.printReceipt()

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -4,6 +4,22 @@ import java.util.List;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
+        List<ItemQuantity> itemQuantityList = getItemQuantity(barcodes);
+        List<ReceiptItem> receiptItemList = buildReceiptItems(itemQuantityList);
+        Receipt receipt = createReceipt(receiptItemList);
+
+        return receipt.toString();
+    }
+
+    private Receipt createReceipt(List<ReceiptItem> receiptItemList) {
+        return null;
+    }
+
+    private List<ReceiptItem> buildReceiptItems(List<ItemQuantity> itemQuantityList) {
+        return null;
+    }
+
+    private List<ItemQuantity> getItemQuantity(List<String> barcodes) {
         return null;
     }
 }
